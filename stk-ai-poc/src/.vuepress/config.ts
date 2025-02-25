8ebdc656d5b5eed6cd1ba4ed05cf2ff688a3f784
Henrique Rodrigues <henrique.rodrigues@zup.com.br>
2025-02-25 14:39:08 -0300

    fix script implementation
    Signed-off-by: Henrique Rodrigues <henrique.rodrigues@zup.com.br>

--- stk-ai-poc/src/.vuepress/config.ts
+++ stk-ai-poc/src/.vuepress/config.ts
@@ -17,19 +17,9 @@ export default defineUserConfig({
   theme,
 
   head: [
-    [
-      "script",
-      {},      
-       ` <script>
-          var _mtm = window._mtm = window._mtm || [];
-          _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-          (function() {
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.async=true; g.src='https://cdn.matomo.cloud/stkpocvuepressthemehopenetlifyapp.matomo.cloud/container_aV0gfOSt.js'; s.parentNode.insertBefore(g,s);
-          })();
-        </script>`
-        ,
-    ],
+    
+    ["script", { async: true,  src: "https://cdn.matomo.cloud/stkpocvuepressthemehopenetlifyapp.matomo.cloud/container_aV0gfOSt.js" }],
+    
   ],
 
   // Enable it with pwa
