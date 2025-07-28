00d2f9733ebca79a34f1133307eb373a5d7cd8ba
Henrique Rodrigues <henrique.rodrigues@zup.com.br>
2025-07-28 14:39:30 -0300

    feat: add mdx doc files and imgs

---
+++ menu-proposal/lib/i18n.ts
@@ -0,0 +1,7 @@
+import type { I18nConfig } from 'fumadocs-core/i18n';
+
+export const i18n: I18nConfig = {
+  defaultLanguage: 'pt-br',
+  languages: ['en', 'pt-br'],
+  hideLocale: 'default-locale',
+};
